715b38f573ec6c2b0cf1e7aa5486a1946828680c
David Odari <davidkibzodari@gmail.com>
2018-10-07 13:43:34 +0300

    [REFACTOR] Created Detail Activity

---
+++ app/src/main/java/ke/co/keki/com/keki/view/DetailActivity.java
@@ -0,0 +1,15 @@
+package ke.co.keki.com.keki.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ke.co.keki.com.keki.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
