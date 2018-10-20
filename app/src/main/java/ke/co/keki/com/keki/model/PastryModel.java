f5a62238547e10c39d9a8a6ffefc534bb946e338
David Odari <davidkibzodari@gmail.com>
2018-10-20 14:01:13 +0300

    [REFACTOR] Convert Class Variables to local variable

--- app/src/main/java/ke/co/keki/com/keki/model/PastryModel.java
+++ app/src/main/java/ke/co/keki/com/keki/model/PastryModel.java
@@ -1,24 +1,7 @@
 package ke.co.keki.com.keki.model;
 
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.provider.Settings;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
-
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.DexterError;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.PermissionRequestErrorListener;
-import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.IOException;
 import java.net.URL;
@@ -32,71 +15,8 @@ import ke.co.keki.com.keki.utils.NetworkUtils;
 
 public class PastryModel implements PastryContract.Model {
 
-    private static final String TAG = PastryModel.class.getSimpleName();
     private static final String JSON_PATH_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
-    LoadPastriesTask loadPastriesTask;
-    public static List<Pastry> pastryList;
-    public boolean isPermissionGranted;
-
-
-    @Override
-    public boolean requestNetworkPermission(final Activity activity) {
-        Dexter.withActivity(activity)
-                .withPermission(
-                        Manifest.permission.ACCESS_NETWORK_STATE
-                )
-                .withListener(new PermissionListener() {
-                    @Override
-                    public void onPermissionGranted(PermissionGrantedResponse response) {
-                        isPermissionGranted = true;
-                    }
-
-                    @Override
-                    public void onPermissionDenied(PermissionDeniedResponse response) {
-                        isPermissionGranted = false;
-                        // check for permanent denial of any permission
-                        if (response.isPermanentlyDenied()) {
-                            // show alert dialog navigating to Settings
-                            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                            builder.setTitle("Need Permissions");
-                            builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
-                            builder.setPositiveButton("GOTO SETTINGS", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                    Uri uri = Uri.fromParts("package", activity.getPackageName(), null);
-                                    intent.setData(uri);
-                                    activity.startActivityForResult(intent, 101);
-                                }
-                            });
-                            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                }
-                            });
-                            builder.show();
-                        }
-                    }
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-                        token.continuePermissionRequest();
-                    }
-                })
-                .withErrorListener(new PermissionRequestErrorListener() {
-                    @Override
-                    public void onError(DexterError error) {
-                        Log.e("Dexter", "There was an error: " + error.toString());
-                    }
-                })
-                .onSameThread()
-                .check();
-
-        return isPermissionGranted;
-    }
-
+    private LoadPastriesTask loadPastriesTask;
 
     /**
      * @param presenter presenter to which changes will be updated and notified
@@ -122,13 +42,12 @@ public class PastryModel implements PastryContract.Model {
         }
     }
 
-
     //AsyncTask
     public static class LoadPastriesTask extends AsyncTask<URL, Void, String> {
 
         private PastryContract.Presenter presenter;
 
-        public LoadPastriesTask(PastryContract.Presenter presenter) {
+        LoadPastriesTask(PastryContract.Presenter presenter) {
             this.presenter = presenter;
         }
 
@@ -141,14 +60,12 @@ public class PastryModel implements PastryContract.Model {
         @Override
         protected String doInBackground(URL... urls) {
             URL Url = urls[0];
-            Log.d(TAG, "searchUrl" + urls[0]);
             String urlJsonResponse = null;
             try {
                 urlJsonResponse = NetworkUtils.getResponseFromHttpUrl(Url);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             return urlJsonResponse;
         }
 
@@ -156,17 +73,14 @@ public class PastryModel implements PastryContract.Model {
         protected void onPostExecute(String strings) {
             super.onPostExecute(strings);
             //JSON Response
-            Log.d(TAG, "jsonResponse:" + strings);
             if (strings != null) {
-                //json response
-                pastryList = JsonUtils.parseJSON(strings);
-                Log.d(TAG, "onDataLoadedItems:" + pastryList.size());
+                List<Pastry> pastryList = JsonUtils.parseJSON(strings);
                 if (pastryList != null) {
                     presenter.getPastryList(pastryList);
                 } else {
                     presenter.onError();
                 }
-            }else {
+            } else {
                 presenter.onError();
             }
         }
