eba80032862f02e6c9cef93bc252d43dacbdb8c6
David Odari <davidkibzodari@gmail.com>
2018-10-20 10:08:10 +0300

    [REFACTOR] Made Dynamic Calculation of columns part of abstract class

--- app/src/main/java/ke/co/keki/com/keki/utils/PastryConstants.java
+++ app/src/main/java/ke/co/keki/com/keki/utils/PastryConstants.java
@@ -1,6 +1,22 @@
 package ke.co.keki.com.keki.utils;
 
+import android.content.Context;
+import android.util.DisplayMetrics;
+
 public abstract class PastryConstants {
     public static String PASTRY = "pastry";
     public static String PASTRY_LIST = "pastry_list";
+    /**
+     * @param context Recycler view instance
+     * @return number of columns for span count
+     */
+    public static int calculateNoOfColumns(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int scaleFactor = 200;
+        int noOfColumns = (int) (dpWidth / scaleFactor);
+        if (noOfColumns == 3)
+            noOfColumns = 2;
+        return noOfColumns;
+    }
 }
