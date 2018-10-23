e36799ae2370474ce21e64fa27a831a6d559fe2d
David Odari <davidkibzodari@gmail.com>
2018-10-23 21:14:14 +0300

    [REFACTOR] Modified Contracts

--- app/src/main/java/ke/co/keki/com/keki/contract/PastryDetailsContract.java
+++ app/src/main/java/ke/co/keki/com/keki/contract/PastryDetailsContract.java
@@ -3,7 +3,10 @@ package ke.co.keki.com.keki.contract;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 
+import java.util.List;
+
 import ke.co.keki.com.keki.model.pojo.Pastry;
+import ke.co.keki.com.keki.model.pojo.Steps;
 
 public interface PastryDetailsContract {
     interface Model {
@@ -12,9 +15,13 @@ public interface PastryDetailsContract {
 
     interface View {
         void bindViews(@NonNull Pastry pastry);
+
+        void openStepView(List<Steps> stepsList);
     }
 
     interface Presenter {
         void onStart(@NonNull Intent intent);
+
+        void onClicked(List<Steps> stepsList);
     }
 }
---
+++ app/src/main/java/ke/co/keki/com/keki/contract/StepsContract.java
@@ -0,0 +1,16 @@
+package ke.co.keki.com.keki.contract;
+
+public interface StepsContract {
+
+    interface Model {
+
+    }
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
