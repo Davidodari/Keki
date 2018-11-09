d8c3c45d018459a4f4ad90f0ccfb97db278d4987
Davidodari <davidkibzodari@gmail.com>
2018-11-09 06:24:09 +0300

    [UPDATE] Removed Default Views Added new views to widget
    - Added TextView and List View

--- app/src/main/java/ke/co/keki/com/keki/IngredientsWidget.java
+++ app/src/main/java/ke/co/keki/com/keki/IngredientsWidget.java
@@ -13,10 +13,9 @@ public class IngredientsWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredients_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
