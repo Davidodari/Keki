d3383efa9167be6eb80bbe10c5597b36b81c9b95
Davidodari <davidkibzodari@gmail.com>
2018-11-16 20:18:12 +0300

    [UPDATE] Updated Widget to display data of selected list

--- app/src/main/java/ke/co/keki/com/keki/widget/IngredientsWidget.java
+++ app/src/main/java/ke/co/keki/com/keki/widget/IngredientsWidget.java
@@ -1,35 +1,67 @@
 package ke.co.keki.com.keki.widget;
 
-
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import java.util.List;
 
 import ke.co.keki.com.keki.R;
+import ke.co.keki.com.keki.model.pojo.Ingredients;
+import ke.co.keki.com.keki.utils.IngredientSharedPreference;
+import ke.co.keki.com.keki.utils.PastryConstants;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class IngredientsWidget extends AppWidgetProvider {
 
+    //List Of Ingredients
+    public static String listOfIngredients;
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
+        SharedPreferences pref = context.getApplicationContext().getSharedPreferences(PastryConstants.SHARED_PREFERENCE_KEY, 0);
+        String recipeIngredientFromPreference = pref.getString(PastryConstants.PREFERENCE_RECIPE_NAME_KEY, context.getResources().getString(R.string.no_desired_ingredient));
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_ingredients_layout);
-        views.setTextViewText(R.id.tv_recipe_name, "Nutella Pie");
-        views.setTextViewText(R.id.tv_ingredients, "500 g pie");
+        //String Builder
+        views.setTextViewText(R.id.tv_recipe_name_widget, context.getResources().getString(R.string.app_name));
+        views.setTextViewText(R.id.tv_ingredients_widget, recipeIngredientFromPreference);
 
-        // Instruct the widget manager to update the widget
+        StringBuilder stringBuilder = new StringBuilder();
+        String recipeNameFromPreference = pref.getString(PastryConstants.PREFERENCE_RECIPE_NAME_KEY, context.getResources().getString(R.string.app_name));
+
+        if (recipeNameFromPreference != null && !recipeNameFromPreference.equals("")) {
+            List<Ingredients> ingredients = IngredientSharedPreference.getIngredientsAsList(context);
+            if (ingredients != null) {
+                for (int i = 0; i < ingredients.size(); i++) {
+                    String ingredient = ingredients.get(i).getIngredientItem();
+                    String measure = ingredients.get(i).getMeasure();
+                    String quantity = String.valueOf(ingredients.get(i).getQuantity());
+
+                    //Build An Ingredient Item
+                    String ingredientItem = ingredient.concat(" " + quantity).concat(" " + measure).concat("\n");
+                    //Append Each Ingredient
+                    stringBuilder.append(ingredientItem);
+                }
+                Log.d("Ingredients List Final", stringBuilder.toString());
+                listOfIngredients = stringBuilder.toString();
+                views.setTextViewText(R.id.tv_recipe_name_widget, recipeNameFromPreference);
+                views.setTextViewText(R.id.tv_ingredients_widget, listOfIngredients);
+            }
+        }
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
+        Toast.makeText(context, "Widget Updated", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -42,4 +74,3 @@ public class IngredientsWidget extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 }
-
